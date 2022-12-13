a8bd0d11815efa3ccfb4eddcb41e536722c4f112
mekail21 <113050730+mekail21@users.noreply.github.com>
2022-12-13 19:43:39 +0600

    Create dum.py

---
+++ dum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#encoding=utf:8
+#dumpingtool 0.0.1
+
+#--Credit Bilal Haider <3
+#---------[ IMPORTING MODULES ]-------->>
+from os import system as cmd
+from string import whitespace
+@@ -41,4 +41,4 @@ def randdumpusr():
+    print(f"[+] Your Username For Dump : {green}{name.lower()}{white}")
+    print("-"*49)
+if __name__=="__main__":
+    randdumpusr()
+    randdumpusr()
